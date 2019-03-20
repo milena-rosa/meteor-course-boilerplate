09ac371e478a980fba3242e0b978b45081122da7
Milena Boselli Rosa <milena.rosa@gmail.com>
2019-03-20 12:12:21 -0300

    Init commit

---
+++ client/main.js
@@ -0,0 +1,16 @@
+import { Meteor } from 'meteor/meteor';
+import { Tracker } from 'meteor/tracker';
+import { Session } from 'meteor/session';
+import ReactDOM from 'react-dom';
+
+import { routes, onAuthChange } from '../imports/routes/routes';
+import '../imports/startup/simple-schema-configuration';
+
+Tracker.autorun(() => {
+    const isAuthenticated = !!Meteor.userId();
+    onAuthChange(isAuthenticated);
+});
+
+Meteor.startup(() => {
+    ReactDOM.render(routes, document.getElementById('app'));
+});
